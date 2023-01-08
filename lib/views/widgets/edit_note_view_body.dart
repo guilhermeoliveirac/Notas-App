efb80f051979ba2191e8e2d3442e1839d3f5d9b0
Guilherme De Oliveira <oliveira.guilherme22@yahoo.com.br>
2023-01-08 11:37:34 -0300

    edit note view

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/views/widgets/custom_app_bar.dart';
+import 'package:note/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key, required this.title});
@@ -19,6 +20,17 @@ class EditNoteViewBody extends StatelessWidget {
             title: "Editar Nota",
             icon: Icons.check,
           ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: 'Title'),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'Escopo',
+            maxLines: 7,
+          )
         ],
       ),
     );
