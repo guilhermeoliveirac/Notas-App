77428c426a7ce8d7dc82cd8dc6f191c014bcb632
Guilherme De Oliveira <oliveira.guilherme22@yahoo.com.br>
2023-01-08 01:02:57 -0300

    include show modal botton

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -6,11 +6,16 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4),
-        child: const NoteItem(),
-      );
-    });
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: const NoteItem(),
+            );
+          }),
+    );
   }
 }
